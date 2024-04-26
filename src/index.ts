3776cc84982693e1f3ca948eac9cc67646a65e4e
0x29a <cartoonbr495@gmail.com>
2024-04-26 15:44:13 -0400

    Patch: connecting on database when initialize

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import dotenv from "dotenv";
+import { SequelizeAuth } from "./database/connection"
 import WebCore from "./web/core";
 
 dotenv.config({
     path: './.env'
 })
 
-new WebCore(Number(process!.env!.port!), express()).start()
+SequelizeAuth.then(() => {
+    new WebCore(Number(process!.env!.port!), express()).start()
+}).catch(() => {
+    throw new Error("Failed to connect to the database.")
+})
