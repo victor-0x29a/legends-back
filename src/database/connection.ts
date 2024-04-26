83d4ac9b85b945c7509ad56d877426cf09a76f4e
0x29a <cartoonbr495@gmail.com>
2024-04-26 15:59:32 -0400

    Patch: exporting the sequelize connection object

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
+export const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: '../../infra/database.db'
 })
