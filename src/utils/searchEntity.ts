9f95e3a6b2d90b6f33627e9dd675bca11f18367b
0x29a <cartoonbr495@gmail.com>
2024-04-27 23:22:59 -0400

    Patch: customMessage param added

--- src/utils/searchEntity.ts
+++ src/utils/searchEntity.ts
@@ -1,15 +1,17 @@
 import type { Model, ModelCtor } from 'sequelize'
 import { LegendHttpError } from '../web/errors'
 
-export const searchEntity = async (model: ModelCtor<Model>, by: any, throwIfExists, throwIfNotExists) => {
+export const searchEntity = async (model: ModelCtor<Model>, by: any, throwIfExists, throwIfNotExists, customMessage = '') => {
     const entity = await model.findOne({ where: by })
 
+    const hasCustomMessage = Boolean(customMessage)
+
     if (entity && throwIfExists) {
-        throw new LegendHttpError(409, 'Entity already exists.')
+        throw new LegendHttpError(409, hasCustomMessage ? customMessage : 'Entity already exists.')
     }
 
     if (!entity && throwIfNotExists) {
-        throw new LegendHttpError(404, 'Entity not found.')
+        throw new LegendHttpError(404, hasCustomMessage ? customMessage : 'Entity not found.')
     }
 
     return entity
