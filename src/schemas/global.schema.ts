1833883c7674620c69e864d1f5fca1b372554cf7
0x29a <cartoonbr495@gmail.com>
2024-04-28 00:10:36 -0400

    Patch: custom errors message to pagination schema

--- src/schemas/global.schema.ts
+++ src/schemas/global.schema.ts
@@ -1,20 +1,20 @@
 import { string, object } from 'yup'
 
 export const paginationSchema = object().shape({
-    page: string().required().test('is-numeric', 'Page must be a number.', (value) => {
+    page: string().required('Page is required').test('is-numeric', 'Page must be a number and greater than zero.', (value) => {
         try {
             const parsed = Number(value)
             return parsed > 0
         } catch (error) {
             return false
         }
-    }),
-    perPage: string().required().test('is-numeric', 'Per page must be a number and greater than zero and less than 50.', (value) => {
+    }).typeError('Page must be a string.'),
+    perPage: string().required('Per page is required.').test('is-numeric', 'Per page must be a number and greater than zero and less than 50.', (value) => {
         try {
             const parsed = Number(value)
             return parsed > 0 && parsed <= 50
         } catch (error) {
             return false
         }
-    })
+    }).typeError('Per page must be a string.')
 })
