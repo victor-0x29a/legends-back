22914a2ce91392d6940efd712f239915062bd43b
0x29a <cartoonbr495@gmail.com>
2024-04-28 00:55:39 -0400

    Feat: loadRouters method on core

--- src/web/core.ts
+++ src/web/core.ts
@@ -2,11 +2,15 @@ import type { Express } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 
+import { Router } from 'express'
+import EntityController from '../controllers/entity.controller'
+
 class WebCore {
     constructor(private readonly port: number, private readonly app: Express) {}
 
     public start(): void {
         this.loadMiddlewares()
+        this.loadRoutes()
         this.app.listen(this.port, () => {
             console.log(`Server is running on port ${this.port}`)
         })
@@ -16,6 +20,10 @@ class WebCore {
         this.app.use(cors({ origin: '*' }))
         this.app.use(bodyParser.json())
         this.app.use(bodyParser.urlencoded({ extended: true }))
+    }
+
+    private loadRoutes(): void {
+        this.app.use('/entity', new EntityController().router)
     }
 }
 
