5b3d2963f76af1b35fd3ee5d3d08e2a443e7508e
0x29a <cartoonbr495@gmail.com>
2024-04-28 01:04:56 -0400

    Feat: exception handler for express

---
+++ src/web/handlerException.ts
@@ -0,0 +1,15 @@
+import * as Yup from 'yup'
+import { NextFunction } from "express"
+import { LegendHttpError } from './errors'
+
+export const HandlerException = (error: any, req, res, next: NextFunction) => {
+    if (error instanceof Yup.ValidationError) {
+        return res.status(400).json({ errorList: error.errors })
+    }
+
+    if (error instanceof LegendHttpError) {
+        return res.status(error.status).json({ errorList: [error.message] })
+    }
+
+    return res.status(500).json({ errorList: ['Internal server error.'] })
+}
