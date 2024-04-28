f18e75f96c2f1e4498ff35a03e0feaad781e97dd
0x29a <cartoonbr495@gmail.com>
2024-04-28 03:16:27 -0400

    Feat: user service with the create method

---
+++ src/services/user.service.ts
@@ -0,0 +1,15 @@
+import { CreateUserDto } from "../dtos/create-user.dto";
+import { UserModel } from "../models/user.model";
+import { searchEntity } from "../utils/searchEntity";
+
+
+class UserService {
+    constructor(private readonly userModel: typeof UserModel) {}
+
+    async create(createUserDto: CreateUserDto) {
+        await searchEntity(this.userModel, { username: createUserDto.username }, true, false, 'User already exists by username.')
+        return await this.userModel.create(createUserDto)
+    }
+}
+
+export { UserService }
