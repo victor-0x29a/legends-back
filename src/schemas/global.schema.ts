44d72085124a518a5fe3589944d5eee4502b8104
0x29a <cartoonbr495@gmail.com>
2024-04-28 02:00:28 -0400

    Refactor: more errors label

--- src/schemas/global.schema.ts
+++ src/schemas/global.schema.ts
@@ -1,4 +1,4 @@
-import { string, object, number } from 'yup'
+import { object, number } from 'yup'
 
 export type parsedPaginationSchema = {
     page: number
@@ -9,15 +9,15 @@ export const paginationSchema = object().shape({
     page: number().required('Page is required').test('is-numeric', 'Page must greater than zero.', (value) => {
         const parsed = Number(value)
         return parsed > 0
-    }),
+    }).typeError('Page must be a number.'),
     perPage: number().required('Per page is required.').test('is-numeric', 'Per page must greater than zero and less than 50.', (value) => {
         const parsed = Number(value)
         return parsed > 0 && parsed <= 50
-    })
+    }).typeError('Per page must be a number.')
 })
 
 export type parsedIdSchema = number
 
 export const idSchema = number().required('Id is required.').test('is-numeric', 'Id must greater than zero.', (value) => {
     return value > 0
-})
+}).typeError('Id must be a number.')
