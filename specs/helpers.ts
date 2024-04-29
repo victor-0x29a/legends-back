60f7b6df52de35f41fc132a98473634608e12fa5
0x29a <cartoonbr495@gmail.com>
2024-04-29 00:13:38 -0400

    Feat: create user method in specs/helper

--- specs/helpers.ts
+++ specs/helpers.ts
@@ -1,5 +1,7 @@
 import { Entity, EntityModel } from '../src/models/entity.model';
 import { faker } from '@faker-js/faker'
+import { User, UserModel } from '../src/models/user.model';
+import * as bcrypt from 'bcrypt'
 
 export const createEntity = async () => {
     const createdEntity = await EntityModel.create({
@@ -17,4 +19,13 @@ export const createEntity = async () => {
         "type": "item"
     }) as unknown as Entity
     return createdEntity
+}
+
+export const createUser = async () => {
+    const hashedPassword = await bcrypt.hash(faker.internet.password(), 10)
+    const createdUser = await UserModel.create({
+        "username": faker.internet.userName(),
+        "password": hashedPassword
+    })
+    return createdUser as unknown as User
 }
