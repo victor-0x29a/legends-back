26c82eae144d2e68fb2fbfe7f7d7b33aff5ee5c1
0x29a <cartoonbr495@gmail.com>
2024-04-28 23:54:28 -0400

    Feat: specs to DELETE /entity/:id

--- specs/entity.e2e.spec.ts
+++ specs/entity.e2e.spec.ts
@@ -92,3 +92,22 @@ describe('POST /entity', () => {
         .expect(400)
     })
 })
+
+describe('DELETE /entity/:id', () => {
+    test('should delete', async () => {
+        const entity = await createEntity()
+        await request(app)
+        .delete(`/entity/${entity.id}`)
+        .expect(204)
+    })
+    test('should reject invalid id', async () => {
+        await request(app)
+        .delete('/entity/invalid')
+        .expect(400)
+    })
+    test('should reject non existent id', async () => {
+        await request(app)
+        .delete('/entity/999999')
+        .expect(404)
+    })
+})
