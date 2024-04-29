b1073dbeed64ddf7dbf8973443feaacc5cfa4b10
0x29a <cartoonbr495@gmail.com>
2024-04-28 23:27:53 -0400

    Feat: specs e2e to GET /entity and GET /entity/:id

---
+++ specs/entity.e2e.spec.ts
@@ -0,0 +1,65 @@
+import request from 'supertest';
+import WebCore from '../src/web/core';
+import express from 'express';
+import { Entity, EntityModel } from '../src/models/entity.model';
+import { faker } from '@faker-js/faker'
+
+const app = new WebCore(3000, express()).app;
+
+const createEntity = async () => {
+    const createdEntity = await EntityModel.create({
+        "title": faker.string.uuid(),
+        "properties": {
+            "strength": 10
+        },
+        "description": "Lorem lorem lorem",
+        "author": "John Doe",
+        "image": {
+            "src": "url",
+            "alt": "alt"
+            },
+        "sections": "markdown content",
+        "type": "item"
+    }) as unknown as Entity
+    return createdEntity
+}
+
+test('GET /entity', async () => {
+    await request(app)
+    .get('/entity?page=1&perPage=10')
+    .expect(200)
+    .expect([])
+})
+
+test('GET /entity without page and perPage', async () => {
+    await request(app)
+    .get('/entity')
+    .expect(400)
+})
+
+test('GET /entity with invalid page and perPage', async () => {
+    await request(app)
+    .get('/entity?page=invalid&perPage=invalid')
+    .expect(400)
+})
+
+test('GET /entity/:id', async () => {
+    const entity = await createEntity()
+    const response = await request(app)
+    .get(`/entity/${entity.id}`)
+
+    expect(response.status).toEqual(200)
+    expect(response.body.title).toEqual(entity.title)
+})
+
+test('GET /entity/:id with invalid id', async () => {
+    await request(app)
+    .get('/entity/invalid')
+    .expect(400)
+})
+
+test('GET /entity/:id with non existent id', async () => {
+    await request(app)
+    .get('/entity/999999')
+    .expect(404)
+})
