092eb10274d359110aaabed02e999a3d3848b86a
0x29a <cartoonbr495@gmail.com>
2024-04-28 21:17:05 -0400

    Feat: find by id route on user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { UserModel } from "../models/user.model";
 import { UserService } from "../services/user.service";
+import { idSchema, parsedIdSchema } from "../schemas/global.schema";
 
 
 class UserController {
@@ -14,11 +15,22 @@ class UserController {
 
     private loadRoutes() {
         this.router.get('/', this.getAll)
+        this.router.get('/:id', this.getById)
     }
 
     private getAll = async (req: Request, res: Response) => {
         const users = await this.Service.findAll()
         return res.status(200).json(users)
+    }
+
+    private getById = async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        const validatedId = await idSchema.validate(id) as unknown as parsedIdSchema
+
+        const entity = await this.Service.findById(validatedId)
+
+        return res.status(200).json(entity)
     }
 }
 
