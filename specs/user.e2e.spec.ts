896bbb820b9f99ace8d65eec08610412a538a9a2
0x29a <cartoonbr495@gmail.com>
2024-04-29 00:25:28 -0400

    Feat: specs to DELETE /user/:id

--- specs/user.e2e.spec.ts
+++ specs/user.e2e.spec.ts
@@ -116,3 +116,22 @@ describe('POST /user', () => {
         .expect(409)
     })
 })
+
+describe('DELETE /user/:id', () => {
+    test('should delete', async () => {
+        const user = await createUser()
+        await request(app)
+        .delete(`/user/${user.id}`)
+        .expect(204)
+    })
+    test('should return 400 with invalid id', async () => {
+        await request(app)
+        .delete('/user/invalid')
+        .expect(400)
+    })
+    test('should return 404 with non existent id', async () => {
+        await request(app)
+        .delete('/user/999999')
+        .expect(404)
+    })
+})
