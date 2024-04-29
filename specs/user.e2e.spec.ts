745a73293d9d6015e3344e0b60338a6586ca3a18
0x29a <cartoonbr495@gmail.com>
2024-04-29 00:24:44 -0400

    Feat: specs to POST /user

--- specs/user.e2e.spec.ts
+++ specs/user.e2e.spec.ts
@@ -32,3 +32,87 @@ test('GET /user/:id with non existent id', async () => {
     .get('/user/999999')
     .expect(404)
 })
+
+describe('POST /user', () => {
+    test('should create', async () => {
+        await request(app)
+        .post('/user')
+        .send({
+            "username": "John Doe",
+            "password": "password"
+        })
+        .expect(201)
+    })
+    test('should return 400 with invalid body', async () => {
+        await request(app)
+        .post('/user')
+        .send({})
+        .expect(400)
+    })
+    test('should return 400 with invalid username', async () => {
+        await request(app)
+        .post('/user')
+        .send({
+            "username": {},
+            "password": "password"
+        })
+        .expect(400)
+    })
+    test('should return 400 with invalid password', async () => {
+        await request(app)
+        .post('/user')
+        .send({
+            "username": "John Doe",
+            "password": {}
+        })
+        .expect(400)
+    })
+    test('should create an user with name', async () => {
+        await request(app)
+        .post('/user')
+        .send({
+            "username": "John Doe 2",
+            "password": "password",
+            "name": "John"
+        })
+        .expect(201)
+
+        const users = await request(app).get('/user')
+
+        const user = users.body.find((user) => user.username === 'John Doe 2')
+
+        expect(user.name).toEqual('John')
+    })
+    test('should create an user with hashed password', async () => {
+        await request(app)
+        .post('/user')
+        .send({
+            "username": "John Doe 3",
+            "password": "password"
+        })
+        .expect(201)
+
+        const users = await request(app).get('/user')
+
+        const user = users.body.find((user) => user.username === 'John Doe 3')
+
+        expect(user.password).not.toEqual('password')
+    })
+    test('should not create an user with duplicated username', async () => {
+        await request(app)
+        .post('/user')
+        .send({
+            "username": "John Doe 4",
+            "password": "password"
+        })
+        .expect(201)
+
+        await request(app)
+        .post('/user')
+        .send({
+            "username": "John Doe 4",
+            "password": "password"
+        })
+        .expect(409)
+    })
+})
