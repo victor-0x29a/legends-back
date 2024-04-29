e037623ebf1d24efd849f96e1a059f80ea2c7f9a
0x29a <cartoonbr495@gmail.com>
2024-04-28 23:12:13 -0400

    Feat: IIFE to load the storage  of database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,17 @@
 import { Sequelize } from "sequelize";
 
+const environment = process.env.NODE_ENV
+
+const storage = function () {
+    if (environment === 'test') {
+        return ':memory:'
+    }
+    return '../../infra/database.db'
+}()
+
 export const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: '../../infra/database.db'
+    storage: storage
 })
 
 export const SequelizeAuth = sequelize.authenticate()
