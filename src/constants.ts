0039b6da17a37b6e494b9625502ab16265b8faca
victor-0x29a <cartoonbr495@gmail.com>
2024-05-16 14:26:50 -0400

    Feat: types of environment has added in constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
-export const isEnableLogging = process.env.NODE_ENV === 'dev'
+export const isDevelopmentEnvironment = process.env.NODE_ENV === 'dev'
+export const isTestingEnvironment = process.env.NODE_ENV === 'test'
+export const isEnableLogging = isDevelopmentEnvironment
