1719a6a2d86e81f5c8bd765ec5fec20f24fb62a5
victor-0x29a <cartoonbr495@gmail.com>
2024-05-17 13:04:58 -0400

    Feat: specs to log service

---
+++ src/services/log.service.spec.ts
@@ -0,0 +1,69 @@
+import { LogModel } from "../models/logs/log.model";
+import { LogService } from "./log.service";
+
+afterEach(async () => {
+    await LogModel.destroy({
+        where: {}
+    })
+})
+
+const service = new LogService()
+
+describe('register', () => {
+    test('should register a log', async () => {
+        const registeredLog = await service.register('test', 'test')
+
+        expect(registeredLog).toBeUndefined()
+    })
+
+    test('should not brake if an error occurs', async () => {
+        // @ts-ignore
+        const registeredLog = await service.register({}, 'test')
+
+        expect(registeredLog).toBeUndefined()
+    })
+})
+
+describe('find', () => {
+    test('find an existing log', async () => {
+        await service.register('test', 'test')
+
+        const existsLogs = await service.find({
+            type: 'test',
+            pagination: {
+                page: 1,
+                perPage: 10
+            }
+        })
+
+        expect(existsLogs).toBeDefined()
+        expect(existsLogs.count).toEqual(1)
+    })
+
+    test('find an existing log without type', async () => {
+        await service.register('test', 'test')
+
+        const existsLogs = await service.find({
+            pagination: {
+                page: 1,
+                perPage: 10
+            }
+        })
+
+        expect(existsLogs).toBeDefined()
+        expect(existsLogs.count).toEqual(1)
+    })
+
+    test('find an non-existing log', async () => {
+        const existsLogs = await service.find({
+            type: 'test',
+            pagination: {
+                page: 1,
+                perPage: 10
+            }
+        })
+
+        expect(existsLogs).toBeDefined()
+        expect(existsLogs.count).toEqual(0)
+    })
+})
