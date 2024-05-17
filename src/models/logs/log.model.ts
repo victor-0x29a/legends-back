ce627aa79ff5addd0ec9906ab5ca66b78e54d502
victor-0x29a <cartoonbr495@gmail.com>
2024-05-17 12:42:00 -0400

    Patch: id field at log model

--- src/models/logs/log.model.ts
+++ src/models/logs/log.model.ts
@@ -7,6 +7,11 @@ export type Log = {
 }
 
 export const LogModel = storageLogsSequelize.define('log', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     type: {
         type: DataTypes.STRING,
         allowNull: false
