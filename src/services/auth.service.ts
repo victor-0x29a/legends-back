4d5af3bc4eac752e0d9bcb4e925a0f81363f4dbb
Victor Gomes <cartoonbr495@gmail.com>
2024-09-27 08:31:48 -0400

    Chore: crypt methods inside of auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,16 @@
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import type { User } from '../models'
 import { InternalValidation } from '../web/errors'
 
 class Auth {
-   constructor (private readonly userData: User, private readonly applicationSecret: string) {}
+    userData: User | null = null
+
+   constructor (private readonly applicationSecret: string = '') {}
+
+    set user (userData: User) {
+        this.userData = userData
+    }
 
     get tokenPayload () {
         return {
@@ -16,6 +23,17 @@ class Auth {
         return {
             expiresIn: '20min'
         }
+    }
+
+    public async getPasswordHash(plainPassword: string): Promise<string> {
+        const salts = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(plainPassword, salts)
+        return passwordHash
+    }
+
+    public async comparePlainTextWithHash(plainText: string, hash: string): Promise<boolean> {
+        const isEqual = await bcrypt.compare(plainText, hash)
+        return isEqual
     }
 
     private validate(): void {
